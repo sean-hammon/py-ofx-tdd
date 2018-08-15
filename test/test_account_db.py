709bd9159b90edfc4eb680a8ade59c7e84537b48
Sean Hammon <git@sean-hammon.com>
2018-08-15 11:29:55 -0600

    test_account_db: first test is passing.

--- test/test_account_db.py
+++ test/test_account_db.py
@@ -1,11 +1,19 @@
+from app.db import TestDb
+
+
+test_db = None
+
+
 def setup():
-    pass
+    global test_db
+    test_db = TestDb.get_connection()
+    print(test_db)
 
 
 def teardown():
+    TestDb.close_connection()
     pass
 
 
-def test_db_open():
-    testdb = None
-    assert testdb is not None
+def test_db_exists():
+    assert test_db is not None
