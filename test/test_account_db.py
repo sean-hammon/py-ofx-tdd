5742170dd3f61fec1b8974f1c27648e8348102a6
Sean Hammon <git@sean-hammon.com>
2018-08-16 10:19:36 -0600

    test_account_db: make sure the database is clean.

--- test/test_account_db.py
+++ test/test_account_db.py
@@ -1,15 +1,27 @@
+from sqlite3 import OperationalError
+
+from nose.tools import raises
+from app.import_file.import_ofx import ImportOfx
 from app.db import TestDb
 
 
 class TestAccountTable:
 
     test_db = None
+    ofx_data = None
 
     def setup(self):
+        importer = ImportOfx('sample.ofx')
+        self.ofx_data = importer.import_ofx()
         self.test_db = TestDb.get_connection()
 
     def teardown(self):
-        self.test_db.close_connection()
+        TestDb.close_connection()
 
     def test_db_exists(self):
         assert self.test_db is not None
+
+    @raises(OperationalError)
+    def test_accounts_table_empty(self):
+        cursor = self.test_db.cursor()
+        cursor.execute('''SELECT * from accounts''')
