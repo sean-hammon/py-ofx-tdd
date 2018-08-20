5a7e1d52c6547b948f17d9a83aa2d1cbc6d94996
Sean Hammon <git@sean-hammon.com>
2018-08-20 10:52:10 -0600

    Add a migration to create the accounts table.

---
+++ sqlite/migrations/20180820-create-accouts-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts(
+     id INTEGER PRIMARY KEY ,
+     alias TEXT,
+     routing_number INTEGER,
+     account_number INTEGER,
+     account_type TEXT,
+     balance REAL,
+     available_balance REAL
+)
